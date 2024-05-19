95edab2e021a137bb23a601fbeeab64c41df1077
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2024-05-19 20:23:19 +0300

    Add files via upload

---
+++ Question.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class Question
+{
+    public string questionText; // Soru metni
+    public string[] options; // Cevap seçenekleri
+    public int correctAnswerIndex; // Doğru cevabın indeksi
+}
